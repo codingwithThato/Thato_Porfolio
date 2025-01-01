b7e6e66d72b78e96025007a51d0b6f932c7b1e3a
Thato Kalagobe <thatokalagobe@proton.me>
2025-01-01 23:04:30 +0200

    created projects page with list of all projects

--- thatokalagobe/app/work/page.jsx
+++ thatokalagobe/app/work/page.jsx
@@ -1,7 +1,14 @@
 // import React from 'react'
 "use client";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGitHub } from "react-icons/bs";
+
+// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { 
   Tooltip, 
@@ -10,8 +17,56 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: 'full-stack (my role: frontend)',
+    title: 'Kicthen of Secrets',
+    description: 'Kitchen of Secrets is a game where you have to cook for supernatural creatures. In this assignment, we were tasked with designing a restaurant simulator, a dynamic blend of activities in the customer floor and the bustling kitchen . On the customer floor, our role is to manage seating, orders, and ensure customer satisfaction. This includes orchestrating the seating process, taking orders, and accommodating customer expectations. Meanwhile, in the kitchen, the chaos of food preparation unfolds. Different chefs handle various aspects, from cooking to plating. Additional features like inventory and accounting can be incorporated to enhance the experience.',
+    stack: [{name: 'React'}, {name: 'C++'}, {name: 'Bootstrap'}, {name: 'React MUI'}],
+  },
+  {
+    num: "02",
+    category: 'Unity game developer',
+    title: 'Mzansi Madness',
+    description: 'Kitchen of Secrets is a game where you have to cook for supernatural creatures. In this assignment, we were tasked with designing a restaurant simulator, a dynamic blend of activities in the customer floor and the bustling kitchen . On the customer floor, our role is to manage seating, orders, and ensure customer satisfaction. This includes orchestrating the seating process, taking orders, and accommodating customer expectations. Meanwhile, in the kitchen, the chaos of food preparation unfolds. Different chefs handle various aspects, from cooking to plating. Additional features like inventory and accounting can be incorporated to enhance the experience.',
+    stack: [{name: 'C#'}, {name: 'Unity'}],
+  },
+  {
+    num: "03",
+    category: 'UX/UI designer',
+    title: 'PantryPal',
+    description: 'Kitchen of Secrets is a game where you have to cook for supernatural creatures. In this assignment, we were tasked with designing a restaurant simulator, a dynamic blend of activities in the customer floor and the bustling kitchen . On the customer floor, our role is to manage seating, orders, and ensure customer satisfaction. This includes orchestrating the seating process, taking orders, and accommodating customer expectations. Meanwhile, in the kitchen, the chaos of food preparation unfolds. Different chefs handle various aspects, from cooking to plating. Additional features like inventory and accounting can be incorporated to enhance the experience.',
+    stack: [{name: 'React'}, {name: 'C++'}, {name: 'Bootstrap'}, {name: 'React MUI'}],
+  },
+  {
+    num: "04",
+    category: 'frontend developer and visual designer',
+    title: 'Flaming Phoenix',
+    description: '...',
+    stack: [{name: 'React'}, {name: 'C++'}, {name: 'Bootstrap'}, {name: 'React MUI'}],
+  },
+  {
+    num: "05",
+    category: 'Unity game developer',
+    title: 'Academia Escape',
+    description: '...',
+    stack: [{name: 'C#'}, {name: 'Unity'}],
+  },
+  {
+    num: "06",
+    category: 'Unity game developer',
+    title: 'NinjaSlice',
+    description: '...',
+    stack: [{name: 'C#'}, {name: 'Unity'}],
+  }
+]
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
+import { name } from "file-loader";
 
 const Work = () => {
     return (
